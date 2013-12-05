c54a077eb5a33c59d1081608172c21d9034a9621
Maciej Chałapuk <m.chalapuk@gmail.com>
2013-12-05 23:53:17 +0100

    + tests for rotor mutex

--- include/gyros/component/rotor_mutex.hpp
+++ include/gyros/component/rotor_mutex.hpp
@@ -8,6 +8,7 @@
 #include <functional>
 #include <vector>
 #include <queue>
+#include <stdexcept>
 
 namespace gyros {
 namespace component {
@@ -31,6 +32,7 @@ class Lock {
 
 template <size_t n_states__ = 3>
 class RotorMutex {
+  static_assert(n_states__ >= 3, "state count must be at least 3");
  public:
   RotorMutex()
       : states_(n_states__) {
@@ -38,6 +40,7 @@ class RotorMutex {
     std::fill(states_.begin(), states_.end(), StateInfo{0, 0});
     for (auto it = states_.begin(),end = states_.end(); it != end; ++it) {
       free_.push(it);
+      it->state_version_ = it - states_.begin();
     }
   }
 
@@ -59,12 +62,21 @@ class RotorMutex {
   std::queue<StateIterator> free_;
 
   StateIterator mostFreshReadOnly() {
+    if (free_.size() == 0) {
+      throw std::logic_error("no free states for writing");
+    }
     auto state = free_.back();
     state->reader_count_ += 1;
     return state;
   }
   StateIterator leastFreshReadWrite() {
+    if (free_.size() == 0) {
+      throw std::logic_error("no free states for writing");
+    }
     auto state = free_.front();
+    if (state->reader_count_ != 0) {
+      throw std::logic_error("no free states for writing");
+    }
     free_.pop();
     return state;
   }
@@ -73,7 +85,7 @@ class RotorMutex {
     state->reader_count_ -= 1;
   }
   void freeAfterWrite(StateIterator state) {
-    state->state_version_ = free_.back().state_version_ + 1;
+    state->state_version_ = free_.back()->state_version_ + 1;
     free_.push(state);
   }
 }; // class RotorMutex<n_states__>
@@ -86,7 +98,7 @@ Lock RotorMutex<n_states__>::acquireReadOnly(size_t *read_index,
   lock.unlock();
 
   *read_index = read_state - states_.begin();
-  *state_version = *read_state;
+  *state_version = read_state->state_version_;
 
   return Lock([this, read_state] () {
                 std::lock_guard<std::mutex> lock(mutex_);
@@ -99,12 +111,12 @@ Lock RotorMutex<n_states__>::acquireReadWrite(size_t *read_index,
                                               size_t *state_version,
                                               size_t *write_index) {
   std::unique_lock<std::mutex> lock(mutex_);
-  StateIterator write_state = leastFreshReadWrite();
   StateIterator read_state = mostFreshReadOnly();
+  StateIterator write_state = leastFreshReadWrite();
   lock.unlock();
 
   *read_index = read_state - states_.begin();
-  *state_version = *read_state;
+  *state_version = read_state->state_version_;
   *write_index = write_state - states_.begin();
 
   return Lock([this, read_state, write_state] () {
---
+++ test/rotor_mutex_test.cpp
@@ -0,0 +1,206 @@
+// author: Maciej Chałapuk
+// license: MIT
+// vim: ts=2 sw=2 expandtab
+#include "gyros/component/rotor_mutex.hpp"
+
+#include <gtest/gtest.h>
+
+using namespace gyros::component;
+using namespace testing;
+
+class component_RotorMutex : public ::testing::TestWithParam<size_t> {
+};
+
+TEST_F(component_RotorMutex,
+       test_same_index_when_acquiring_readonly_twice) {
+  RotorMutex<3> mutex;
+  size_t read_index0, read_index1, unused;
+  auto lock0 = mutex.acquireReadOnly(&read_index0, &unused);
+  auto lock1 = mutex.acquireReadOnly(&read_index1, &unused);
+  ASSERT_EQ(read_index0, read_index1);
+}
+
+TEST_F(component_RotorMutex,
+       test_same_state_version_when_acquiring_readonly_twice) {
+  RotorMutex<3> mutex;
+  size_t state_version0, state_version1, unused;
+  auto lock0 = mutex.acquireReadOnly(&unused, &state_version0);
+  auto lock1 = mutex.acquireReadOnly(&unused, &state_version1);
+  ASSERT_EQ(state_version0, state_version1);
+}
+
+TEST_F(component_RotorMutex,
+       test_same_index_when_acquiring_readonly_thrice) {
+  RotorMutex<3> mutex;
+  size_t read_index0, read_index1, read_index2, unused;
+  auto lock0 = mutex.acquireReadOnly(&read_index0, &unused);
+  auto lock1 = mutex.acquireReadOnly(&read_index1, &unused);
+  auto lock2 = mutex.acquireReadOnly(&read_index2, &unused);
+  ASSERT_EQ(read_index0, read_index1);
+  ASSERT_EQ(read_index0, read_index2);
+}
+
+TEST_F(component_RotorMutex,
+       test_same_state_version_when_acquiring_readonly_thrice) {
+  RotorMutex<3> mutex;
+  size_t state_version0, state_version1, state_version2, unused;
+  auto lock0 = mutex.acquireReadOnly(&unused, &state_version0);
+  auto lock1 = mutex.acquireReadOnly(&unused, &state_version1);
+  auto lock2 = mutex.acquireReadOnly(&unused, &state_version2);
+  ASSERT_EQ(state_version0, state_version1);
+  ASSERT_EQ(state_version0, state_version2);
+}
+
+TEST_F(component_RotorMutex,
+       test_same_read_index_when_acquiring_readonly_and_readwrite) {
+  RotorMutex<3> mutex;
+  size_t read_index0, read_index1, unused;
+  auto lock0 = mutex.acquireReadOnly(&read_index0, &unused);
+  auto lock1 = mutex.acquireReadWrite(&read_index1, &unused, &unused);
+  ASSERT_EQ(read_index0, read_index1);
+}
+
+TEST_F(component_RotorMutex,
+       test_same_state_version_when_acquiring_readonly_and_readwrite) {
+  RotorMutex<3> mutex;
+  size_t state_version0, state_version1, unused;
+  auto lock0 = mutex.acquireReadOnly(&unused, &state_version0);
+  auto lock1 = mutex.acquireReadWrite(&unused, &state_version1, &unused);
+  ASSERT_EQ(state_version0, state_version1);
+}
+
+TEST_F(component_RotorMutex,
+       test_same_read_index_when_acquiring_readwrite_and_readonly) {
+  RotorMutex<3> mutex;
+  size_t read_index0, read_index1, unused;
+  auto lock1 = mutex.acquireReadWrite(&read_index1, &unused, &unused);
+  auto lock0 = mutex.acquireReadOnly(&read_index0, &unused);
+  ASSERT_EQ(read_index0, read_index1);
+}
+
+TEST_F(component_RotorMutex,
+       test_same_state_version_when_acquiring_readwrite_and_readonly) {
+  RotorMutex<3> mutex;
+  size_t state_version0, state_version1, unused;
+  auto lock1 = mutex.acquireReadWrite(&unused, &state_version1, &unused);
+  auto lock0 = mutex.acquireReadOnly(&unused, &state_version0);
+  ASSERT_EQ(state_version0, state_version1);
+}
+
+TEST_F(component_RotorMutex,
+       test_different_read_and_write_indices_when_acquiring_readwrite) {
+  RotorMutex<3> mutex;
+  size_t read_index, write_index, unused;
+  auto lock0 = mutex.acquireReadWrite(&read_index, &unused, &write_index);
+  ASSERT_NE(read_index, write_index);
+}
+
+TEST_F(component_RotorMutex,
+       test_different_write_index_when_acquiring_rw_after_releasing_rw_lock) {
+  RotorMutex<3> mutex;
+  size_t write_index0, write_index1, unused;
+  {
+    auto lock0 = mutex.acquireReadWrite(&unused, &unused, &write_index0);
+  }
+  auto lock1 = mutex.acquireReadWrite(&unused, &unused, &write_index1);
+  ASSERT_NE(write_index0, write_index1);
+}
+
+TEST_F(component_RotorMutex,
+       test_different_read_index_when_acquiring_rw_after_releasing_rw_lock) {
+  RotorMutex<3> mutex;
+  size_t read_index0, read_index1, unused;
+  {
+    auto lock0 = mutex.acquireReadWrite(&read_index0, &unused, &unused);
+  }
+  auto lock1 = mutex.acquireReadWrite(&read_index1, &unused, &unused);
+  ASSERT_NE(read_index0, read_index1);
+}
+
+TEST_F(component_RotorMutex,
+       test_different_state_version_when_acquiring_rw_after_releasing_rw_lock) {
+  RotorMutex<3> mutex;
+  size_t state_version0, state_version1, unused;
+  {
+    auto lock0 = mutex.acquireReadWrite(&unused, &state_version0, &unused);
+  }
+  auto lock1 = mutex.acquireReadWrite(&unused, &state_version1, &unused);
+  ASSERT_NE(state_version0, state_version1);
+}
+
+TEST_F(component_RotorMutex,
+       test_same_read_index_after_full_rw_cycle) {
+  RotorMutex<3> mutex;
+  size_t read_index0, read_index1, unused;
+  {
+    auto lock0 = mutex.acquireReadWrite(&read_index0, &unused, &unused);
+  }
+  {
+    auto lock1 = mutex.acquireReadWrite(&unused, &unused, &unused);
+  }
+  {
+    auto lock2 = mutex.acquireReadWrite(&unused, &unused, &unused);
+  }
+  auto lock3 = mutex.acquireReadWrite(&read_index1, &unused, &unused);
+  ASSERT_EQ(read_index0, read_index1);
+}
+
+TEST_F(component_RotorMutex,
+       test_same_write_index_after_full_rw_cycle) {
+  RotorMutex<3> mutex;
+  size_t write_index0, write_index1, unused;
+  {
+    auto lock0 = mutex.acquireReadWrite(&unused, &unused, &write_index0);
+  }
+  {
+    auto lock1 = mutex.acquireReadWrite(&unused, &unused, &unused);
+  }
+  {
+    auto lock2 = mutex.acquireReadWrite(&unused, &unused, &unused);
+  }
+  auto lock3 = mutex.acquireReadWrite(&unused, &unused, &write_index1);
+  ASSERT_EQ(write_index0, write_index1);
+}
+TEST_F(component_RotorMutex,
+       test_shorter_rw_cycle_if_not_releasing_ro_lock) {
+  RotorMutex<3> mutex;
+  size_t write_index0, write_index1, unused;
+  auto lock0 = mutex.acquireReadOnly(&unused, &unused);
+  {
+    auto lock1 = mutex.acquireReadWrite(&unused, &unused, &write_index0);
+  }
+  {
+    auto lock2 = mutex.acquireReadWrite(&unused, &unused, &unused);
+  }
+  auto lock3 = mutex.acquireReadWrite(&unused, &unused, &write_index1);
+  ASSERT_EQ(write_index0, write_index1);
+}
+
+TEST_F(component_RotorMutex,
+       test_state_version_diff_after_full_rw_cycle) {
+  RotorMutex<3> mutex;
+  size_t state_version0, state_version1, unused;
+  {
+    auto lock0 = mutex.acquireReadWrite(&unused, &state_version0, &unused);
+  }
+  {
+    auto lock1 = mutex.acquireReadWrite(&unused, &unused, &unused);
+  }
+  {
+    auto lock2 = mutex.acquireReadWrite(&unused, &unused, &unused);
+  }
+  auto lock3 = mutex.acquireReadWrite(&unused, &state_version1, &unused);
+  ASSERT_EQ(state_version0 + 3, state_version1);
+}
+
+TEST_F(component_RotorMutex,
+       test_throw_when_full_locking_rw) {
+  RotorMutex<3> mutex;
+  size_t unused;
+  auto lock0 = mutex.acquireReadWrite(&unused, &unused, &unused);
+  auto lock1 = mutex.acquireReadWrite(&unused, &unused, &unused);
+  ASSERT_THROW({ mutex.acquireReadWrite(&unused, &unused, &unused); },
+               std::logic_error);
+}
+
+
