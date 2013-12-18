62214ca3522ac638b69709e96c2b10923362d19e
Maciej Chałapuk <maciej@chalapuk.pl>
2013-12-18 23:33:49 +0100

    ~ AssertIsSame instead of std::is_same

--- test/type_list_test.cpp
+++ test/type_list_test.cpp
@@ -14,6 +14,11 @@ using Mock = test::gyros::component::MockComponent;
 
 using namespace gyros::util::type_list;
 
+template <class Expected, class Actual>
+struct AssertIsSame {
+  static_assert(std::is_same<Expected, Actual>::value, "type are not same");
+}; // struct AssertIsSame<Expected, Actual>
+
 /*
  * All tests in this file are executed during compile time ^^
  * All functions are static (not visible outside this file) and never called.
@@ -44,52 +49,44 @@ static_assert(
     );
 void testGettingEightTest() {
   typedef TypeList<Mock, Mock, Mock, Mock, Mock, Mock, Mock, Simple> List;
-  static_assert(std::is_same<typename Get<List, 7>::Type, Simple>::value,
-                "test getting eight type");
+  AssertIsSame<typename Get<List, 7>::Type, Simple>();
 }
 
 // push back
 
 static void testPushingBackToEmptyList() {
   typedef typename PushBack<TypeList<>, Simple>::Type AfterPush;
-  static_assert(std::is_same<AfterPush, TypeList<Simple>>::value,
-              "test pushing back to empty type list");
+  AssertIsSame<AfterPush, TypeList<Simple>>();
 }
 static void testPushingBackToNotEmptyList() {
   typedef typename PushBack<TypeList<Simple>, Simple>::Type AfterPush;
-  static_assert(std::is_same<AfterPush, TypeList<Simple, Simple>>::value,
-              "test pushing back to not empty type list");
+  AssertIsSame<AfterPush, TypeList<Simple, Simple>>();
 }
 
 // cat
 
 static void testConcatenatingTwoEmptyLists() {
   typedef typename Cat<TypeList<>, TypeList<>>::Type Concatenated;
-  static_assert(std::is_same<Concatenated, TypeList<>>::value,
-                "test concatenating two empty lists");
+  AssertIsSame<Concatenated, TypeList<>>();
 }
 static void testConcatenatingNotEmptyListWithEmptyList() {
   typedef typename Cat<TypeList<Simple>, TypeList<>>::Type Concatenated;
-  static_assert(std::is_same<Concatenated, TypeList<Simple>>::value,
-                "test concatenating not empty list with empty list");
+  AssertIsSame<Concatenated, TypeList<Simple>>();
 }
 static void testConcatenatingEmptyListWithNotEmptyList() {
   typedef typename Cat<TypeList<>, TypeList<Simple>>::Type Concatenated;
-  static_assert(std::is_same<Concatenated, TypeList<Simple>>::value,
-                "test concatenating not empty list with empty list");
+  AssertIsSame<Concatenated, TypeList<Simple>>();
 }
 static void testConcatenatingTwoSingletonLists() {
   typedef typename Cat<TypeList<Simple>, TypeList<Simple>>::Type Concatenated;
-  static_assert(std::is_same<Concatenated, TypeList<Simple, Simple>>::value,
-                "test concatenating two singleton lists");
+  AssertIsSame<Concatenated, TypeList<Simple, Simple>>();
 }
 static void testConcatenatingTwoListsWithTwoTypes() {
   typedef TypeList<Mock, Mock> FirstList;
   typedef TypeList<Simple, Simple> SecondList;
   typedef TypeList<Mock, Mock, Simple, Simple> ExpectedList;
   typedef typename Cat<FirstList, SecondList>::Type Concatenated;
-  static_assert(std::is_same<Concatenated, ExpectedList>::value,
-                "test concatenating two lists with two types");
+  AssertIsSame<Concatenated, ExpectedList>();
 }
 static void testConcatenatingThreeSingletonLists() {
   typedef TypeList<Mock> FirstList;
@@ -97,8 +94,7 @@ static void testConcatenatingThreeSingletonLists() {
   typedef TypeList<Member<int>> ThirdList;
   typedef TypeList<Mock, Simple, Member<int>> ExpectedList;
   typedef typename Cat<FirstList, SecondList, ThirdList>::Type Concatenated;
-  static_assert(std::is_same<Concatenated, ExpectedList>::value,
-                "test concatenating three singleton lists");
+  AssertIsSame<Concatenated, ExpectedList>();
 }
 
 // permute
@@ -111,16 +107,8 @@ static_assert(Size<typename Permute<TypeList<Simple>>::Type>::value == 1,
 static void testCreatingPermutationsOfOneTypeList() {
   typedef TypeList<TypeList<Simple>, TypeList<Member<int>>> ExpectedList;
   typedef typename Permute<TypeList<Simple, Member<int>>>::Type ActualList;
-  static_assert(std::is_same<ExpectedList, ActualList>::value,
-                "test creating permutations of one typeList");
+  AssertIsSame<ExpectedList, ActualList>();
 }
-
-template <class ExpectedType, class ActualType>
-struct AssertIsSame {
-  static_assert(std::is_same<ExpectedType, ActualType>::value,
-                "type are not same");
-}; // struct AssertIsSame<ExpectedType, ActualType>
-
 static void testCreatingCartesianProductOfTypeListWithItself() {
   typedef TypeList<
       TypeList<Simple, Simple>,
@@ -174,13 +162,11 @@ static void testCreatingCartesianProductOfThreeTypeLists() {
 static void testCreatingPowerSetFromEmptyTypeList() {
   typedef TypeList<TypeList<>> ExpectedSets;
   typedef typename PowerSet<TypeList<>>::Type ActualSets;
-  static_assert(std::is_same<ActualSets, ExpectedSets>::value,
-                "test creating power set from empty type list");
+  AssertIsSame<ActualSets, ExpectedSets>();
 }
 static void testCreatingPowerSetFromSingletonTypeList() {
   typedef TypeList<TypeList<>, TypeList<Simple>> ExpectedSets;
   typedef typename PowerSet<TypeList<Simple>>::Type ActualSets;
-  static_assert(std::is_same<ActualSets, ExpectedSets>::value,
-                "test creating power set from empty type list");
+  AssertIsSame<ActualSets, ExpectedSets>();
 }
 
